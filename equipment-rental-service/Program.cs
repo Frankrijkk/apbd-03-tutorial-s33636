a85579996f823cb775092fb8acaedade5da77ae1
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-03-22 21:13:57 +0100

    add the skeleton of CLI

--- equipment-rental-service/Program.cs
+++ equipment-rental-service/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace equipment_rental_service;
+﻿using System.Globalization;
 
-class Program
+namespace equipment_rental_service;
+
+class CommandLineInterface
 {
-    static void Main(string[] args)
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to the PAJTK equipment rental service");
+        int choice = chooseMenu(1, "What would you like to do?\n1. PLACEHOLDER\n0. exit");
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("s");
+                break;
+            case 0:
+                return;
+        }
+    }
+
+    private static int chooseMenu(int max, string message)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.WriteLine(message);
+            string choice = Console.ReadLine();
+            try
+            {
+                int choiceIndex = int.Parse(choice);
+                if (choiceIndex >= 0 && choiceIndex <= max)
+                {
+                    return choiceIndex;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid option");
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid option");
+            }
+        }
     }
 }
